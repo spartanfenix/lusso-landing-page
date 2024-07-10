05e26fcc8271f13089fe2871b3579a6c101a9adf
Iván Eduardo <31447148+spartanfenix@users.noreply.github.com>
2024-07-10 15:50:42 -0600

    Feature: FontFamily edited to Montserrat

--- src/router.tsx
+++ src/router.tsx
@@ -7,21 +7,31 @@ import Contact from "./components/pages/Contact";
 import Error404 from "./components/pages/Error404";
 import DrawerAppBar from "./components/DrawerAppBar/DrawerAppBar";
 import Footer from "./components/Footer/Footer";
+import "@fontsource/montserrat/200.css";
+import "@fontsource/montserrat/300.css";
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/montserrat/700.css";
+import theme from "./theme";
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
 
 const AppRouter = () => {
   return (
-    <BrowserRouter>
-      <DrawerAppBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/packets" element={<Packets />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<Error404 />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <DrawerAppBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/packets" element={<Packets />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
