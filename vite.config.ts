9c95332a62bc3bb55cbdfb99b68be80509a2a2ee
Iván Eduardo <31447148+spartanfenix@users.noreply.github.com>
2024-07-16 01:11:39 -0600

    Fixed: vite.config.ts edited

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: './robots.txt',
-          dest: '.'
-        },
-        {
-          src: './sitemap.xml',
-          dest: '.'
-        }
-      ],
-    })
-  ], 
-  base: "https://lussohighmusic.com.mx/"
+  plugins: [react()],
+  // base: "https://spartanfenix.github.io/lusso-landing-page",
 });
