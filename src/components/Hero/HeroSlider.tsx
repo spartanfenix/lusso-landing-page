acd1663359eb4ec19bf871a4823fbd7760787a12
Iván Eduardo <31447148+spartanfenix@users.noreply.github.com>
2025-08-20 22:28:22 -0600

    Antes de actualizar MUI a v7

--- src/components/Hero/HeroSlider.tsx
+++ src/components/Hero/HeroSlider.tsx
@@ -2,6 +2,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { Box, useMediaQuery } from "@mui/material";
+
 import Portada1 from "../../assets/img/portadaInicio1.png";
 import Portada2 from "../../assets/img/portadaInicio2.png";
 import Portada3 from "../../assets/img/portadaInicio3.png";
@@ -9,11 +10,18 @@ import Texto1 from "../../assets/img/textoPortada1.png";
 import Texto2 from "../../assets/img/textoPortada2.png";
 import Texto3 from "../../assets/img/textoPortada3.png";
 
+import Portada1Movil from "../../assets/img/portadaInicio1Movil.jpg";
+import Portada2Movil from "../../assets/img/portadaInicio2Movil.jpg";
+import Portada3Movil from "../../assets/img/portadaInicio3Movil.jpg";
+import Texto1Movil from "../../assets/img/textoPortada1Movil.png";
+import Texto2Movil from "../../assets/img/textoPortada2Movil.png";
+import Texto3Movil from "../../assets/img/textoPortada3Movil.png";
+
 // Configuración del slider
 const settings = {
   dots: true,
   infinite: true,
-  speed: 5000,
+  speed: 500,
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
@@ -25,20 +33,27 @@ const slides = [
   {
     background: Portada1,
     textImage: Texto1,
+    backgroundMovil: Portada1Movil,
+    textImageMovil: Texto1Movil,
   },
   {
     background: Portada2,
     textImage: Texto2,
+    backgroundMovil: Portada2Movil,
+    textImageMovil: Texto2Movil,
   },
   {
     background: Portada3,
     textImage: Texto3,
+    backgroundMovil: Portada3Movil,
+    textImageMovil: Texto3Movil,
   },
 ];
 
 const HeroSlider = () => {
   const isMobile = useMediaQuery("(max-width:600px)");
-  const isTablet = useMediaQuery("(max-width:1365px)");
+  const isTablet = useMediaQuery("(max-width:1279px)");
+
   return (
     <Box
       style={{
@@ -52,26 +67,27 @@ const HeroSlider = () => {
           <Box
             key={index}
             sx={{
-              width: "100vw",
-              height: isMobile ? "25vh" : isTablet ? "50vh" : "100vh",
-              backgroundImage: `url(${slide.background})`,
+              width: "100%",
+              height: isTablet ? "100vh" : "100vh",
+              backgroundImage: `url(${
+                isMobile ? slide.backgroundMovil : slide.background
+              })`,
               backgroundRepeat: "no-repeat",
               backgroundSize: "cover",
-              backgroundPosition:
-                isMobile || isTablet ? "center top" : "center center",
+              backgroundPosition: "center center",
               position: "relative",
             }}
           >
             <Box
               component="img"
-              src={slide.textImage}
+              src={isMobile ? slide.textImageMovil : slide.textImage}
               alt="Texto portada"
               sx={{
                 position: "absolute",
                 top: "50%",
                 left: "50%",
                 transform: "translate(-50%, -50%)",
-                width: isMobile ? "50%" : "80%",
+                width: isMobile ? "85%" : "80%",
                 maxWidth: "600px",
               }}
             />
--- src/components/pages/Gallery.tsx
+++ src/components/pages/Gallery.tsx
@@ -6,47 +6,47 @@ import SliderPhotos from "../Sliders/SliderPhotos";
 import HeroVideo from "../Hero/HeroVideo";
 
 const Gallery = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-    return () => clearTimeout(timer);
-  }, []);
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
+        return () => clearTimeout(timer);
+    }, []);
 
-  return (
-    <>
-      {isLoading ? (
-        <Loading />
-      ) : (
+    return (
         <>
-          <HeroVideo />
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              maxHeight: "100vh",
-              my: "2rem",
-            }}
-          >
-            <SliderPhotos />
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              maxHeight: "100vh",
-              my: "2rem ",
-            }}
-          >
-            <SliderYoutube />
-          </Box>
+            {isLoading ? (
+                <Loading />
+            ) : (
+                <>
+                    {/* <HeroVideo /> */}
+                    <Box
+                        sx={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            maxHeight: "100vh",
+                            my: "2rem",
+                        }}
+                    >
+                        {/* <SliderPhotos /> */}
+                    </Box>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            maxHeight: "100vh",
+                            my: "2rem ",
+                        }}
+                    >
+                        <SliderYoutube />
+                    </Box>
+                </>
+            )}
         </>
-      )}
-    </>
-  );
+    );
 };
 
 export default Gallery;
